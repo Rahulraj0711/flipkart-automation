4a0d4ebf83541700601bf5c2d68fa3a5f619c098
Rahul Raj <justrahulronaldo1999@gmail.com>
2024-05-13 02:19:38 +0530

    First Commit By- Rahul

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,13 +1,17 @@
 package demo;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
+
+    @BeforeSuite(alwaysRun = true)
+    public void InitializeDriver()
     {
         System.out.println("Constructor: TestCases");
         WebDriverManager.chromedriver().timeout(30).setup();
@@ -15,6 +19,7 @@ public class TestCases {
         driver.manage().window().maximize();
     }
 
+    @AfterSuite
     public void endTest()
     {
         System.out.println("End Test: TestCases");
@@ -22,13 +27,11 @@ public class TestCases {
         driver.quit();
 
     }
-
     
+    @Test
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
         System.out.println("end Test case: testCase02");
     }
-
-
 }
